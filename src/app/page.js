49e640e3c2d0be6351e1ef8190189f3f2d6eb63b
larr <sadpandaemoji@gmail.com>
2023-07-05 20:14:12 -0400

    add search and filter ui elements

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,56 @@
 "use client";
 // import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import CountryCard from "@/components/CountryCard";
 
 export default function Home() {
   const [countries, setCountries] = useState([]);
+  const [searchField, setSearchField] = useState("search field");
+  const [filterField, setFilterField] = useState("filter field");
+
+  function handleSearch() {}
+  function handleFilter() {}
 
   const fetchCountries = async () => {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const data = await response.json();
     setCountries(data);
-    console.log(JSON.stringify(data));
+    // console.log(JSON.stringify(data));
   };
 
   fetchCountries();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <input className="border" type="text"></input>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="flex flex-col justify-between mx-auto border">
+        {/* my thinking is, mutate the countries array?
+        or, create a copy, that renders if searchField exists, and hides the main one, if it doesnt.*/}
+        {/* <label for="search">Search</label> */}
+        <input
+          id="search"
+          name="search"
+          className="border"
+          type="text"
+          placeholder="Search for a country..."
+          onChange={(e) => setSearchField(e.target.value)}
+          value={searchField}
+        ></input>
+        <p>{searchField}</p>
+        {/* <label for="filter">Filter</label> */}
+        <select
+          name="filter"
+          id="filter"
+          onChange={(e) => setFilterField(e.target.value)}
+        >
+          <option>Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+        <p>{filterField}</p>
+      </div>
       {countries &&
         countries.map((country, index) => (
           <CountryCard key={index} data={country} />
