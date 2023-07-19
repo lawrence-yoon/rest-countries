bdd8374cf2b236dbe852606984b1193df53d379a
larr <sadpandaemoji@gmail.com>
2023-07-18 22:48:56 -0400

    add random page

---
+++ src/app/country/random/page.js
@@ -0,0 +1,86 @@
+"use client";
+
+import React, { useEffect } from "react";
+import useLocalStorage from "@/components/hooks/useLocalStorage";
+import Header from "@/components/Header";
+import useToggle from "@/components/hooks/useToggle";
+import CountryDetailsCard from "@/components/CountryDetailsCard";
+import Link from "next/link";
+import { FetchDataButton } from "@/components/ui/FetchDataButton";
+
+export default function CountryRandomPage() {
+  const [countries, setCountries] = useLocalStorage("data", []);
+  const [isDarkToggled, setIsDarkToggled] = useToggle("isDark", false);
+
+  function handleData(data) {
+    setCountries(data);
+    localStorage.setItem("data", JSON.stringify(countries));
+  }
+
+  const countryRandom = countries[Math.floor(Math.random() * countries.length)];
+  const country = countryRandom;
+  // const countryCode = country.cca3;
+  const countryNativeName =
+    country && country.name.nativeName
+      ? Object.values(country.name.nativeName)
+      : null;
+
+  function borderCountry(inputString) {
+    const countryByCode = countries.filter(
+      (item) => item.cca3.toLowerCase() === inputString.toLowerCase()
+    );
+    const outputString = countryByCode[0].name.common;
+    return outputString;
+  }
+
+  const countryBorders =
+    country && country.borders
+      ? country.borders.map((border) => borderCountry(border))
+      : [];
+
+  const countryCurrencies =
+    country && country.currencies
+      ? Object.values(country.currencies).map((item) => item.name + " ")
+      : [];
+
+  const countryLanguages =
+    country && country.languages
+      ? Object.values(country.languages).map((item) => item + " ")
+      : [];
+
+  const countryDetails = {
+    name: country ? country.name.common : null,
+    nativeName: countryNativeName ? countryNativeName[0].common : null,
+    flag: country ? country.flags.svg : null,
+    alt: country ? country.flags.alt : null,
+    population: country ? country.population.toLocaleString("en-US") : null,
+    region: country ? country.region : null,
+    subRegion: country ? country.subregion : null,
+    capital: country ? country.capital : null,
+    //tld, currencies, languages, bordercountries, are arrays.
+    topLevelDomain: country ? country.tld[0] : null,
+    currencies: countryCurrencies,
+    languages: countryLanguages,
+    borderCountries: countryBorders,
+  };
+
+  return (
+    <main className={`${isDarkToggled ? "dark" : ""}`}>
+      <Header handleToggle={setIsDarkToggled} />
+      <div className="flex flex-col gap-4 px-8 pt-20 pb-4 dark:text-white dark:bg-dark-blue-1 min-h-screen">
+        <Link
+          className="w-fit p-2 px-4 border-transparent rounded-md dark:bg-dark-blue-0"
+          href="/"
+        >
+          &lt; Back
+        </Link>
+        {countryDetails ? (
+          <CountryDetailsCard {...countryDetails} />
+        ) : (
+          <p>problem</p>
+        )}
+        <p>{country ? null : <FetchDataButton setData={handleData} />}</p>
+      </div>
+    </main>
+  );
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
 "use client";
+import { useRouter } from "next/navigation";
 import Moon from "./ui/icons/Moon";
 import { MoonSolid } from "./ui/icons/Moon";
 
 function Header({ handleToggle = () => {} }) {
+  const router = useRouter();
+  function handleRandom() {
+    router.push("/country/random");
+    window.location.reload();
+  }
   return (
     <header>
       <div className="fixed top-0 left-0 right-0 flex flex-row justify-between text-dark-blue-2 bg-white dark:bg-dark-blue-0 dark:text-white">
         <h2 className="font-bold p-4 whitespace-nowrap">Where in the world?</h2>
         {/* {isDarkToggled ? <p>dark on</p> : <p>light on</p>} */}
         <button
-          className="flex flex-row px-6 py-4 gap-3 whitespace-nowrap"
+          className="flex flex-row px-3 py-4 gap-3 whitespace-nowrap shadow-sm hover:opacity-75 hover:scale-125"
+          onClick={handleRandom}
+        >
+          ?
+        </button>
+        <button
+          className="flex flex-row px-6 py-4 gap-3 whitespace-nowrap shadow-sm hover:opacity-75"
           onClick={handleToggle}
         >
           <Moon />
